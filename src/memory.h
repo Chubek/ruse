908b60178c07ef7ffc1d67ab241838136136f62d
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-08-12 07:01:14 +0330

    Revised: the heap object

--- src/memory.h
+++ src/memory.h
@@ -8,8 +8,16 @@
 
 #include "object.h"
 
-void gc_collect (environ_t *env);
-void gc_mark (object_t *obj);
-void gc_sweep (object_t *obj);
+typedef struct Heap
+{
+  object_t *local_stack;
+  object_t *global_roots;
+  object_t *curr_env_chain;
+  object_t *curr_conti;
+} heap_t;
+
+void heap_collect (heap_t *heap);
+void heap_mark (object_t *obj);
+void heap_sweep (object_t *obj);
 
 #endif
